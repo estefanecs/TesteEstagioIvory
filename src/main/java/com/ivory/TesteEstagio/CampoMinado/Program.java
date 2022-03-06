7bd21977670eda169e50dde5572dfa37477b530f
Estéfane Souza <estefanescs@gmail.com>
2022-03-06 10:22:50 -0300

    ajeitando o código

--- src/main/java/com/ivory/TesteEstagio/CampoMinado/Program.java
+++ src/main/java/com/ivory/TesteEstagio/CampoMinado/Program.java
@@ -10,12 +10,9 @@ public class Program {
         System.out.println(campoMinado.Tabuleiro());
 
         // Realize sua codificação a partir deste ponto, boa sorte!
-
         while (campoMinado.JogoStatus().equals(StatusTipo.Aberto)) {
-            //Analisa qual posição será aberta
-            int[] posicao = this.analisarPosicao(campoMinado);
-            //Abre a melhor posição(linha,coluna)
-            campoMinado.Abrir(posicao[0] + 1, posicao[1] + 1);
+            int[] posicao = this.analisarPosicao(campoMinado); //Analisa qual posição será aberta
+            campoMinado.Abrir(posicao[0] + 1, posicao[1] + 1); //Abre a melhor posicao(linha,coluna)
             System.out.println("\nPosição escolhida: " + posicao[0] + "," + posicao[1]);
             //Exibe o tabuleiro após a escolha da posição
             System.out.println("\n*****Atualização do tabuleiro*****");
@@ -28,7 +25,6 @@ public class Program {
         } else {
             System.out.println("\n****GAME OVER! TENTE NOVAMENTE.****");
         }
-
     }
 
     // Função que analisa qual posição deverá ser aberta
@@ -46,209 +42,182 @@ public class Program {
             }
         }
 
-        //APARTIR DAQUI ESTÁ ERRADO
-        int l = 0, c = 0;
+        int l = 0, c;
         boolean isBomba;
         boolean encontrou;
         while (l < 9) {
-            System.out.println("\n l=" + l);
             c = 0;
             while (c < 9) {
-                System.out.println("\n************ ESTOU EM c=" + c);
                 isBomba = false;
                 encontrou = false;
-                System.out.println("\n valor do tabuleiro: " + tabuleiro[l][c]);
+                int risco = 0;
                 if ("-".equals(tabuleiro[l][c])) {
-                    System.out.println("\nentrei na analise de -: posicao " + l + c);
                     //Posição da casa atual
                     posicao[0] = l;
                     posicao[1] = c;
+                    String valorCasa;
 
                     //LINHA ABAIXO, MESMA COLUNA
                     if (l != 8) {
-                        System.out.println("\n entrei em l!8");
-                        String a = tabuleiro[l + 1][c];
-                        if (a.equals("0")) {
-                            System.out.println("\n retornei em l!0 e c!8");
+                        valorCasa = tabuleiro[l + 1][c];
+                        if (valorCasa.equals("0")) {
                             return posicao;
                         } else {
                             isBomba = true;
                             encontrou = false;
                         }
-                        System.out.println("\n valor de c" + c+ " valor de encontrou= "+encontrou);
                     }
 
                     //LINHA ABAIXO, COLUNA Á DIREITA
                     if (l != 8 && c != 8) {
-                        System.out.println("\n entrei em= l!8 e c!8");
-                        String b = tabuleiro[l + 1][c + 1];
-                        if (b.equals("0")) {
-                            System.out.println("\n retornei em l!0 e c!8");
+                        valorCasa = tabuleiro[l + 1][c + 1];
+                        if (valorCasa.equals("0")) {
                             return posicao;
-                        } else if (tabuleiro[l][c + 1].equals("-") && c+2<=8) {
+                        } else if (tabuleiro[l][c + 1].equals("-") && c + 2 <= 8) {
                             if (tabuleiro[l + 1][c + 2].equals("0")) {
                                 posicao[0] = l;
                                 posicao[1] = c + 1;
-                                encontrou = true;
-                                isBomba = false;
-                                System.out.println("\r\n posicao nova: " + l + c + 1);
                                 return posicao;
 
-                            } //funciona em casos que a lateral não existe, poderia retornar direto
-                            else if (!"-".equals(tabuleiro[l + 1][c + 2])) {
-                                posicao[0] = l;
-                                posicao[1] = c;
+                            } else if (!"-".equals(tabuleiro[l + 1][c + 2])) {
                                 isBomba = false;
                                 encontrou = true;
-                                System.out.println("\r\n posicao atual: " + l + c);
 
                             }
                         }
-                        System.out.println("\n valor de c" + c+ " valor de encontrou= "+encontrou);
                     }
 
                     //LINHA ABAIXO, COLUNA Á ESQUERDA
                     if (l != 8 && c != 0) {
-                        System.out.println("\n entrei em l!8 e c!0=");
-                        String d = tabuleiro[l + 1][c - 1];
-                        if (d.equals("0")) {
-                            System.out.println("\n retornei em l!0 e c!8");
+                        valorCasa = tabuleiro[l + 1][c - 1];
+                        if (valorCasa.equals("0")) {
                             isBomba = false;
                             encontrou = true;
-                            //return posicao;
                         } else {
                             isBomba = true;
                         }
-                        System.out.println("\n valor de c" + c+ " valor de encontrou= "+encontrou);
                     }
 
                     //LINHA ACIMA, MESMA COLUNA
                     if (l != 0) {
-                        System.out.println("\n entrei em l!0=");
-                        String e = tabuleiro[l - 1][c];
-                        if (e.equals("0")) {
-                            System.out.println("\n retornei em l!0 e c!8");
+                        valorCasa = tabuleiro[l - 1][c];
+                        if (valorCasa.equals("0")) {
                             encontrou = true;
-                            //return posicao;
                             isBomba = false;
 
                         } else {
                             isBomba = true;
                         }
-                        System.out.println("\n valor de c" + c+ " valor de encontrou= "+encontrou);
                     }
 
                     //LINHA ACIMA, COLUNA Á DIREITA
                     if (l != 0 && c != 8) {
-                        System.out.println("\n entrei em l!0 e c!8");
-                        String f = tabuleiro[l - 1][c + 1];
-                        if (f.equals("0")) {
-                            System.out.println("\n retornei em l!0 e c!8");
+                        valorCasa = tabuleiro[l - 1][c + 1];
+                        if (valorCasa.equals("0")) {
                             encontrou = true;
-                            //return posicao;
-                            isBomba = false;
-                        } else if (isBomba == true && tabuleiro[l][c + 1].equals("-") && c+2<=8) {
+                            return posicao;
+                        } else if (isBomba && tabuleiro[l][c + 1].equals("-") && c + 2 <= 8) {
                             if (tabuleiro[l - 1][c + 2].equals("0")) {
                                 posicao[0] = l;
                                 posicao[1] = c + 1;
-                                encontrou = true;
-                                isBomba = false;
-                                System.out.println("\r\n posicao nova: " + l + c + 1);
                                 return posicao;
 
-                            } //funciona em casos que a lateral não existe, poderia retornar direto
-                            else if (!"-".equals(tabuleiro[l - 1][c + 2])) {
-                                posicao[0] = l;
-                                posicao[1] = c;
+                            } else if (!"-".equals(tabuleiro[l - 1][c + 2])) {
                                 isBomba = false;
                                 encontrou = true;
-                                System.out.println("\r\n posicao atual: " + l + c);
                             }
-                            
                         }
-                        if(encontrou && c==0){
-                            System.out.println("retornei por aqui ");
+                        if (encontrou && c == 0) {
                             return posicao;
                         }
-                        System.out.println("\n valor de c" + c+ " valor de encontrou= "+encontrou +"analisando LINHA ACIMA, COLUNA Á DIREITA");
                     }
 
                     //LINHA ACIMA, COLUNA Á ESQUERDA
                     if (l != 0 && c != 0) {
-                        System.out.println("\n entrei em l!0 e c!0");
-                        String g = tabuleiro[l - 1][c - 1];
-                        if (g.equals("0")) {
-                            System.out.println("\n retornei em l!0 e c!0");
+                        valorCasa = tabuleiro[l - 1][c - 1];
+                        if (valorCasa.equals("0")) {
                             encontrou = true;
                             return posicao;
-                        } else if (tabuleiro[l][c - 1].equals("-") && c-2>=0) {
+                        } else if (tabuleiro[l][c - 1].equals("-") && c - 2 >= 0) {
                             if (tabuleiro[l - 1][c - 2].equals("0")) {
                                 posicao[0] = l;
                                 posicao[1] = c - 1;
-                                encontrou = true;
-                                isBomba = false;
-                                System.out.println("\r\n posicao nova: " + l + c + 1);
                                 return posicao;
 
-                            } //funciona em casos que a lateral não existe, poderia retornar direto
-                            else if (!"-".equals(tabuleiro[l - 1][c - 2])) {
-                                posicao[0] = l;
-                                posicao[1] = c;
+                            } else if (!"-".equals(tabuleiro[l - 1][c - 2])) {
                                 isBomba = false;
                                 encontrou = true;
-                                System.out.println("\r\n achei posicao atual: " + l + c);
                             }
-                            
+
                         }
-                        if(encontrou && c==9){
+                        if (encontrou && c == 9) {
                             return posicao;
-                        }
-                        else{
+                        } else {
                             encontrou = false;
                         }
-                        System.out.println("\n valor de c" + c+ " valor de encontrou= "+encontrou);
+
                     }
 
                     //MESMA LINHA, E UMA COLUNA Á ESQUERDA
                     if (c != 0) {
-                        System.out.println("\n entrei em c!0=");
-                        String e = tabuleiro[l][c - 1];
-                        if (e.equals("0")) {
-                            System.out.println("\n retornei em  c!0");
+                        valorCasa = tabuleiro[l][c - 1];
+                        if (valorCasa.equals("0")) {
                             encontrou = true;
                             isBomba = false;
 
-                        } else{
+                        } else if (valorCasa.equals("-") && c - 2 >= 0) {
+                            if (tabuleiro[l][c - 2].equals("0")) {
+                                posicao[0] = l;
+                                posicao[1] = c - 1;
+                                return posicao;
+
+                            } else if (!"-".equals(tabuleiro[l][c - 2])) {
+
+                                if (c == 9 && !isBomba) {
+                                    encontrou = true;
+                                }
+                                isBomba = false;
+                            }
+
+                        } else {
+                            encontrou = false;
                             isBomba = true;
                         }
-                        System.out.println("\n valor de c" + c+ " valor de encontrou= "+encontrou);
                     }
 
                     //MESMA LINHA, E UMA COLUNA Á DIREITA
                     if (c != 8) {
-                        System.out.println("\n entrei em c!8=");
-                        String e = tabuleiro[l][c + 1];
-                        if (e.equals("0")) {
-                            System.out.println("\n retornei em  c!8");
+                        valorCasa = tabuleiro[l][c + 1];
+                        if (valorCasa.equals("0")) {
                             encontrou = true;
                             isBomba = false;
 
+                        } else if (valorCasa.equals("-") && c + 2 <= 8) {
+                            if (tabuleiro[l][c + 2].equals("0")) {
+                                posicao[0] = l;
+                                posicao[1] = c + 1;
+                                return posicao;
+
+                            } else if (!"-".equals(tabuleiro[l][c + 2])) {
+                                if (c == 0 && !isBomba) {
+                                    encontrou = true;
+                                }
+                                isBomba = false;
+
+                            }
                         } else {
+                            encontrou = false;
                             isBomba = true;
                         }
-                        System.out.println("\n valor de c" + c + " valor de encontrou= "+encontrou);
                     }
-
-                    if (encontrou == true) {
+                    if (encontrou) {
                         return posicao;
                     }
                 }
-
                 c++;
             }
             l++;
         }
         return posicao;
-
     }
 }
